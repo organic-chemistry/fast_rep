8742c9f937aacad97eb553d97c1ac7037da42bca
Arbona <jeanmichel.arbona@ens-lyon.fr>
2025-05-21 10:09:11 +0200

    correct analyse models

--- workflows/analyse-models-synthetic-data.sh
+++ workflows/analyse-models-synthetic-data.sh
@@ -15,7 +15,7 @@ for model in Weibull Exponential; do
         for fit_mode in Laplace; do
             # Construct the output file name with the chromosome
             for data in Weibull_True Weibull_False Exponential_True Exponential_False; do
-                for noise in 0.2 0.1 0.05
+                for noise in 0.2 0.1 0.05; do
                     common="2500 20  --signal-to-fit theo_rfd --regions chrI"
                     output_file="input${data}_${fit_mode}-${model}_${fit_time}_${noise}_bayesian.bed"
                     bayesian synthetic/chrI-${data}.bed analysis_models/$output_file $common --fit-mode $fit_mode --noise $noise --model-type $model  $fit_time_arg  & 
